6e7f5d180197d33a30d7a49e02ac3a87ecbd5254
ivanbilkis <ivanbilkis@gmail.com>
2023-10-28 14:04:26 -0300

    nueva interfaz para validar
    --

---
+++ src/logica/DatosProducto.java
@@ -0,0 +1,30 @@
+
+	package logica;
+
+	import javax.swing.JOptionPane;
+
+	public interface DatosProducto {
+
+	    public default boolean nombrePorducto (String nombre) {
+
+	        if (nombre.isEmpty() || nombre.length()<=1 ||  nombre.contains("@") ||nombre.contains(".")  ||nombre.contains("?")  || nombre.contains(",")) {
+
+	            JOptionPane.showMessageDialog(null, "Nombre de producto ingresado incorrecrtamente");
+	            return false;
+	        }else {
+	            return true;
+
+	        }
+	    }
+
+	    public default boolean pesoProducto (Double peso) {
+
+	        if (peso==0) {
+	            JOptionPane.showMessageDialog(null, "Error en el peso ingresado");
+	        return false;
+	        }
+	        return true;
+
+	    }
+	}
+
